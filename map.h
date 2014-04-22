2ba9a6a7a3874521e994bfa9cd36fbc83512d9af
Adrien Guinet <aguinet@quarkslab.com>
2014-04-22 23:24:18 +0200

    level3, SFINAE

--- map.h
+++ map.h
@@ -10,6 +10,11 @@ struct has_reserve
     static constexpr bool value = sizeof(Test<T>(0)) == sizeof(char);
 };
 
+template <typename T>
+struct has_random_access
+{
+	static constexpr bool value = 
+};
 
 template <class Container>
 inline typename std::enable_if<has_reserve<Container>::value == true, void>::type map_reserve(Container& c, size_t const n)
@@ -29,6 +34,28 @@ Container map(Func const& f, Container const& c)
 	map_reserve(ret, c.size());
 	for (auto const& v: c) {
 		ret.push_back(f(v));
+	}
+	return ret;
+}
+
+template <class Func, class Container>
+std::enable_if<has_random_access<Container>::value == false, Container>::type map(Func const& f, Container const& c)
+{
+	Container ret;
+	map_reserve(ret, c.size());
+	for (auto const& v: c) {
+		ret.push_back(f(v));
+	}
+	return ret;
+}
+
+template <class Func, class Container>
+std::enable_if<has_random_access<Container>::value == true, Container>::type map(Func const& f, Container const& c)
+{
+	Container ret;
+	ret.resize(c.size());
+	for (size_t i = 0; i < c.size(); i++) {
+		ret[i] = f(c[i]);
 	}
 	return ret;
 }
