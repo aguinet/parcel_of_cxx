2d29558b7a84e9391e312db99bb6749e9e0a4b67
Adrien Guinet <aguinet@quarkslab.com>
2014-04-22 23:45:15 +0200

    Level4

--- bench.cpp
+++ bench.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 
 #include "map.h"
+#include "string.h"
 
 template <class Container>
 void set(Container& ar, const size_t N)
@@ -25,7 +26,6 @@ void __attribute__ ((noinline)) bench(const char* name, Args&& ... args)
 
 	const size_t size_data = sizeof(typename decltype(ret)::value_type) * ret.size();
 	const double time = (end.tv_sec+end.tv_usec/1000000.0)-(start.tv_sec+start.tv_usec/1000000.0);
-	std::cout << *ret.begin() << std::endl;
 	std::cout << name << ": " << (time*1000.0) << " ms, BW: " << (2*size_data)/(time*1024.0*1024.0) << " MB/s" << std::endl;
 }
 
@@ -45,6 +45,12 @@ int main(int argc, char** argv)
 	std::list<float> ar2;
 	set(ar2, N);
 	bench("list", [](float v) { return 2.0f*v+4.0f; }, ar2);
+
+	std::vector<String> strs;
+	for (size_t i = 0; i < N; i++) {
+		strs.emplace_back(String("my little poney"));
+	}
+	bench("str", [](String const& s) { return std::move(s.to_upper()); }, strs);
 
 	return 0;
 }
--- map.h
+++ map.h
@@ -10,11 +10,9 @@ struct has_reserve
     static constexpr bool value = sizeof(Test<T>(0)) == sizeof(char);
 };
 
-template <typename T>
-struct has_random_access
-{
-	static constexpr bool value = 
-};
+template <typename Container>
+struct has_random_access: public std::is_same<typename std::iterator_traits<typename Container::const_iterator>::iterator_category, std::random_access_iterator_tag>
+{ };
 
 template <class Container>
 inline typename std::enable_if<has_reserve<Container>::value == true, void>::type map_reserve(Container& c, size_t const n)
@@ -28,36 +26,25 @@ inline typename std::enable_if<has_reserve<Container>::value == false, void>::ty
 }
 
 template <class Func, class Container>
-Container map(Func const& f, Container const& c)
+typename std::enable_if<has_random_access<Container>::value == false, Container>::type map(Func const& f, Container const& c)
 {
 	Container ret;
 	map_reserve(ret, c.size());
 	for (auto const& v: c) {
-		ret.push_back(f(v));
+		ret.emplace_back(std::move(f(v)));
 	}
-	return ret;
+	return std::move(ret);
 }
 
 template <class Func, class Container>
-std::enable_if<has_random_access<Container>::value == false, Container>::type map(Func const& f, Container const& c)
-{
-	Container ret;
-	map_reserve(ret, c.size());
-	for (auto const& v: c) {
-		ret.push_back(f(v));
-	}
-	return ret;
-}
-
-template <class Func, class Container>
-std::enable_if<has_random_access<Container>::value == true, Container>::type map(Func const& f, Container const& c)
+typename std::enable_if<has_random_access<Container>::value == true, Container>::type map(Func const& f, Container const& c)
 {
 	Container ret;
 	ret.resize(c.size());
 	for (size_t i = 0; i < c.size(); i++) {
-		ret[i] = f(c[i]);
+		ret[i] = std::move(f(c[i]));
 	}
-	return ret;
+	return std::move(ret);
 }
 
 #endif
