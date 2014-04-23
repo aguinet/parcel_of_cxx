6e40db061543aab538e3a9a29f75df130f0369e0
Adrien Guinet <aguinet@quarkslab.com>
2014-04-23 14:13:12 +0200

    level4

--- map.h
+++ map.h
@@ -1,10 +1,31 @@
 #ifndef MAP_H
 #define MAP_H
 
+template<typename T>
+struct has_reserve
+{
+    template<typename U, size_t (U::*)() const> struct SFINAE {}; 
+    template<typename U> static char Test(SFINAE<U, &U::reserve>*);
+    template<typename U> static int Test(...);
+    static constexpr bool value = sizeof(Test<T>(0)) == sizeof(char);
+};
+
+template <class Container>
+void map_reserve(Container& c, typename std::enable_if<has_reserve<Container>::value == true, size_t>::type s)
+{
+	c.reserve(s);
+}
+
+template <class Container>
+void map_reserve(Container&, typename std::enable_if<has_reserve<Container>::value == false, size_t>::type)
+{
+}
+
 template <class Func, class Container>
 Container map(Func const& f, Container const& c)
 {
 	Container ret;
+	map_reserve(ret, c.size());
 	for (auto const& v: c) {
 		ret.emplace_back(std::move(f(v)));
 	}
