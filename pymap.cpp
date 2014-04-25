35fe0b9125599fc1bb62172574d57eb4b4b88c4d
Adrien Guinet <aguinet@quarkslab.com>
2014-04-25 17:45:01 +0200

    boost python examples

--- pymap.cpp
+++ pymap.cpp
@@ -2,12 +2,39 @@
 #include <boost/python.hpp>
 #include <boost/python/stl_iterator.hpp>
 #include <cstdint>
+#include <memory>
 
 #include "map.h"
 
 using namespace boost::python;
 
-list map_u32(object& f, list const& l)
+struct myobj
+{
+	myobj()
+	{
+		std::cout << "constructor" << std::endl;
+	}
+	
+	~myobj()
+	{
+		std::cout << "destructor" << std::endl;
+	}
+
+	static void* operator new(std::size_t sz)
+	{
+		std::cout << "custom new for size " << sz << '\n';
+		return ::operator new(sz);
+	}
+
+	static void operator delete(void* ptr)
+	{
+		std::cout << "custom placement delete called" << std::endl;
+		::operator delete(ptr);
+	}
+};
+
+
+list map_u32(object& f, list& l)
 {
 	stl_input_iterator<uint32_t> begin(l), end;
 	std::vector<uint32_t> buf(begin, end);
@@ -15,17 +42,31 @@ list map_u32(object& f, list const& l)
 			[&f](uint32_t const v)
 			{
 				object ret = f(v);
-				return extract<uint32_t>(f(v))();
+				return boost::python::extract<uint32_t>(f(v))();
 			},
 			buf, seq_backend());
-	list ret;
+	list ret; 
 	for (uint32_t v: mapped) {
 		ret.append(v);
 	}
 	return ret;
 }
 
+myobj* new_obj()
+{
+	return new myobj();
+}
+
+std::shared_ptr<myobj> new_shared_obj()
+{
+	return std::shared_ptr<myobj>(new myobj());
+}
+
 BOOST_PYTHON_MODULE(pymap)
 {
 	def("map_u32", &map_u32);
+	def("new_obj", &new_obj, return_value_policy<manage_new_object>());
+	def("new_shared_obj", &new_shared_obj);
+
+	class_<myobj, std::shared_ptr<myobj>>("myobj");
 }
