1a272236d1e7caf3efede876fc8146c543f640b8
Adrien Guinet <aguinet@quarkslab.com>
2014-05-01 14:07:28 +0200

    Boost python

--- pymap.cpp
+++ pymap.cpp
@@ -8,32 +8,6 @@
 
 using namespace boost::python;
 
-struct myobj
-{
-	myobj()
-	{
-		std::cout << "constructor" << std::endl;
-	}
-	
-	~myobj()
-	{
-		std::cout << "destructor" << std::endl;
-	}
-
-	static void* operator new(std::size_t sz)
-	{
-		std::cout << "custom new for size " << sz << '\n';
-		return ::operator new(sz);
-	}
-
-	static void operator delete(void* ptr)
-	{
-		std::cout << "custom placement delete called" << std::endl;
-		::operator delete(ptr);
-	}
-};
-
-
 list map_u32(object& f, list& l)
 {
 	stl_input_iterator<uint32_t> begin(l), end;
@@ -52,21 +26,7 @@ list map_u32(object& f, list& l)
 	return ret;
 }
 
-myobj* new_obj()
-{
-	return new myobj();
-}
-
-std::shared_ptr<myobj> new_shared_obj()
-{
-	return std::shared_ptr<myobj>(new myobj());
-}
-
 BOOST_PYTHON_MODULE(pymap)
 {
 	def("map_u32", &map_u32);
-	def("new_obj", &new_obj, return_value_policy<manage_new_object>());
-	def("new_shared_obj", &new_shared_obj);
-
-	class_<myobj, std::shared_ptr<myobj>>("myobj");
 }
